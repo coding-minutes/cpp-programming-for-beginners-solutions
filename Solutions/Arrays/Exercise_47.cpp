c17a3a7c7b8a8a29739773db565f7119bc4fdaee
Dhruvil Soni <dhruvilsoni1703@gmail.com>
2024-08-31 17:55:38 +0530

    Update Exercise_47.cpp

--- Solutions/Arrays/Exercise_47.cpp
+++ Solutions/Arrays/Exercise_47.cpp
@@ -1,7 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int calculateMedian(int A[], int sizeOfArray) {
-    
-    return A[((sizeOfArray-1) / 2)];
+float calculateMedian(int A[], int sizeOfArray) {
+    if (sizeOfArray % 2 == 1) {
+        // For odd-sized array, return the middle element
+        return A[sizeOfArray / 2];
+    } else {
+        // For even-sized array, return the average of the two middle elements
+        int mid1 = sizeOfArray / 2 - 1;
+        int mid2 = sizeOfArray / 2;
+        return (A[mid1] + A[mid2]) / 2.0;
+    }
 }
